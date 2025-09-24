e23db878422dc8b6ece0022a19e4c17425be2669
estiver <estiver@ELOM0JX9RN34-N.elo.corp>
2025-09-23 21:28:50 -0300

    ajustes  no template

---
+++ python-app/template/src/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+from datetime import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/info')
+def info():
+    return jsonify({
+        'time': datetime.now(),
+        'hostname': socket.gethostname(),
+        'message': 'You are doing great, human! :)',
+        'deployed_on': 'kubernetes',
+        'env': '${{values.app_env}}',
+        'app_name': '${{values.app_name}}'
+    })
+
+@app.route('/api/v1/healthz')
+def health():
+    return jsonify({
+        'status': 'up'
+    }), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
+# '/api/v1/details'
+# '/api/vi/heathz'
